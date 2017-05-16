174632a7a889478a0a5c2942fbcd5f01151464c0
wuzhizhe <wuzhizhemu569@gmail.com>
2017-05-16 17:48:54 +0800

    new content

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const Koa = require('koa');
-const router = require('./router/index');
 const views = require('koa-views');
+const opn = require('opn');
+const serve = require('koa-static');
 const app = new Koa();
+const router = require('./router/index');
 const config = require('./config');
 
 app.use(views(__dirname + '/views', { extension: 'pug' }))
+
+app.use(serve(__dirname + '/node_modules/bootstrap.native/dist'));
 
 app
   .use(router.routes())
   .use(router.allowedMethods())
   .listen(config.httpPort)
+
+opn('http://127.0.0.1:' + config.httpPort)
